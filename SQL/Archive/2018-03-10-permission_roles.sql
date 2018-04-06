b43eabae45c299fa7936c12ecb41d8e00947750a
Rida CCNA <ridz_1208@hotmail.com>
2018-04-05 21:50:28 -0400

    new world
    clean-up
    
    moved file

--- SQL/Archive/2018-03-10-permission_roles.sql
+++ SQL/Archive/2018-03-10-permission_roles.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `role` (
  `RoleID` INTEGER unsigned NOT NULL AUTO_INCREMENT,
- `Name` varchar(255),
- `Label` varchar(255),
+ `RoleName` varchar(191),
+ `RoleLabel` varchar(191),
  PRIMARY KEY (`RoleID`),
- UNIQUE KEY `UK_Name` (`Name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ UNIQUE KEY `UK_Name` (`RoleName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `role_permission_rel` (
   `RoleID` INTEGER unsigned NOT NULL,
@@ -12,12 +12,12 @@ CREATE TABLE `role_permission_rel` (
   PRIMARY KEY  (`RoleID`,`PermissionID`),
   CONSTRAINT `FK_role_permission_rel_RoleID` FOREIGN KEY (`RoleID`) REFERENCES `role` (`RoleID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_role_permission_rel_PermissionID` FOREIGN KEY (`PermissionID`) REFERENCES `permissions` (`permID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `user_role_rel` (
   `UserID` INTEGER unsigned NOT NULL,
   `RoleID` INTEGER unsigned NOT NULL,
   PRIMARY KEY  (`UserID`,`RoleID`),
-  CONSTRAINT `FK_user_role_rel_userID` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_user_role_rel_RoleID` FOREIGN KEY (`RoleID`) REFERENCES `role` (`RoleID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  CONSTRAINT `FK_user_role_rel_userID` FOREIGN KEY (`UserID`) REFERENCES `users` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_user_role_rel_RoleID` FOREIGN KEY (`RoleID`) REFERENCES `role` (`RoleID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
