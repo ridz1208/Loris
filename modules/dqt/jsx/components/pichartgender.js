6494c735dc89ed1e84cb98efa0a871439d6e2430
Alizée Bellerose <misotheism@users.noreply.github.com>
2020-10-14 19:12:10 -0400

    fix colours

--- modules/dqt/jsx/components/pichartgender.js
+++ modules/dqt/jsx/components/pichartgender.js
@@ -31,9 +31,7 @@ const PiChartGender = (props) => {
       },
       color: {
           pattern: [
-            '#bc77af', '#009ff1', '#2DC3D0', '#4AE8C2', '#D90074', '#7900DB', '#FF8000',
-            '#0FB500', '#CC0000', '#DB9CFF', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2',
-            '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'
+            '#f4e2f0', '#0c6db0',
           ]
       }
     });
